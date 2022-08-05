0071f9f4f9d1bb9bfe1c9bd13fe2bb78ed220e97
Migheli <98918370+Migheli@users.noreply.github.com>
2022-08-05 10:06:19 +0300

    Add tinymce text redactor to admin

--- places/models.py
+++ places/models.py
@@ -1,13 +1,17 @@
 from django.db import models
+from tinymce.models import HTMLField
+
+
 print('hello world')
 # Create your models here.
+
 
 
 class Place(models.Model):
     id = models.CharField(max_length=5)
     title = models.CharField(max_length=200, primary_key=True)
     description_short = models.TextField('Краткое описание')
-    description_long = models.TextField('Подробное описание')
+    description_long = HTMLField('Подробное описание')
     lat = models.FloatField('Широта')
     lon = models.FloatField('Долгота')
 
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 #from where_to_go import views
 from places import views
 
@@ -26,5 +26,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.show_main_page),
-    path('places/<int:place_id>', views.show_place_detail)
+    path('places/<int:place_id>', views.show_place_detail),
+    path('tinymce/', include('tinymce.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
