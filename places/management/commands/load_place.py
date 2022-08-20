5c4953d617f3b69bf928ad885c856133e5c984e9
Migheli <avanesyan.migel@gmail.com>
2022-08-20 10:39:10 +0300

    Update load_place.py
    Set update_or_create method, add automatic old imgs delete if place object alreadey exists

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -7,6 +7,35 @@ import json
 import os
 
 
+def update_or_create_place(place_dataset):
+    place, created = Place.objects.update_or_create(
+        title=place_dataset['title'],
+        defaults={
+            'description_short': place_dataset['description_short'],
+            'description_long': place_dataset['description_long'],
+            'lon': place_dataset['coordinates']['lng'],
+            'lat': place_dataset['coordinates']['lat'],
+        }
+    )
+    if not created:
+        place.photos.all().delete()
+    related_images = []
+    for img_number, img_url in enumerate(place_dataset['imgs'], 1):
+        image = requests.get(img_url)
+        image_binary_content = BytesIO(image.content).read()
+        image_content_file = ContentFile(image_binary_content)
+        current_image = Image(
+            location=place,
+        )
+        current_image.photo.save(
+            f'{img_number} {place.title}.jpg',
+            image_content_file,
+            save=False)
+
+        related_images.append(current_image)
+    Image.objects.bulk_create(related_images)
+
+
 class Command(BaseCommand):
     help = 'Creating or update a place model from JSON-file'
 
@@ -17,6 +46,7 @@ class Command(BaseCommand):
         file_path = options['place_dir'][0]
         if file_path.startswith('http'):
             place_dataset = requests.get(file_path).json()
+            update_or_create_place(place_dataset)
         else:
             for root, dirs, files in os.walk(file_path):
                 for file in files:
@@ -24,28 +54,4 @@ class Command(BaseCommand):
                         continue
                     with open(os.path.join(root, file), encoding="utf8") as place_file:
                         place_dataset = json.load(place_file)
-
-
-        place, created = Place.objects.get_or_create(
-            title=place_dataset['title'],
-            description_short=place_dataset['description_short'],
-            description_long=place_dataset['description_long'],
-            lon=place_dataset['coordinates']['lng'],
-            lat=place_dataset['coordinates']['lat'],
-        )
-        related_images = []
-        for img_number, img_url in enumerate(place_dataset['imgs'], 1):
-            image = requests.get(img_url)
-            image_binary_content = BytesIO(image.content).read()
-            image_content_file = ContentFile(image_binary_content)
-            current_image = Image(
-                title=f'{place.title}{img_number} ',
-                location=place,
-            )
-            current_image.photo.save(
-                f'{img_number} {place.title}.jpg',
-                image_content_file,
-                save=False)
-
-            related_images.append(current_image)
-        Image.objects.bulk_create(related_images)
+                        update_or_create_place(place_dataset)
