9f5354c202121d2b056c3eb1b706cfbfe57bc8c3
Migheli <avanesyan.migel@gmail.com>
2022-08-21 13:23:39 +0300

    Update urls.py
    Set DEBUG-relative media data settings

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -15,14 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from places import views
-
 from django.conf import settings
 from django.conf.urls.static import static
-from environs import Env
-
-env = Env()
-env.read_env()
+from places import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,4 +25,7 @@ urlpatterns = [
     path('places/<int:place_id>', views.show_place_detail),
     path('tinymce/', include('tinymce.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
